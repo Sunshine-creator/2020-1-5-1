4415e8abea8b280e76bb55adc3170f4793bf5421
Sunshine-creator <941389138@163.com>
2020-01-05 22:53:02 +0800

    java练习

---
+++ Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    public String name;
+    public  int age;
+}
+class Test{
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person.name);
+        System.out.println(person.age);
+    }
+}
+
+//    public int age;
+//    public  String name;
+//    public String sex;
+//    public  void eat(){
+//        System.out.println("吃饭！");
+//    }
+//    public void sleep(){
+//        System.out.println("睡觉！");
+//    }
+//
+//    public static void main(String[] args) {
+//        Person person  =new Person();
+//        person.eat();
+//        person.sleep();
+//    }
+
---
+++ Test.java
@@ -0,0 +1,8 @@
+ class Test {
+    public static void main(String[] args) {
+        Person person =new Person();
+        System.out.println(peson.name);
+        System.out.println(person.age);
+    }
+}
+
